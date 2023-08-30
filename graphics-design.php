5961ab9228ecd0cc5d7daa70a786b2aaf152bfb4
DanroyNdungu <danroy.mwangi@strathmore.edu>
2023-08-30 22:17:25 +0300

    feat:add graphics design portfolio

---
+++ graphics-design.php
@@ -0,0 +1,243 @@
+<?php $title="graphics"; ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Graphics Design Portfolio.</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="sty.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+    <link rel="manifest" href="./site.webmanifest">
+
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css'>
+    <link rel="stylesheet" href="./slider/style.css">
+</head>
+<body class="bg-three">
+    <?php include_once('./partials/nav.php') ?>
+    <section class="main hero md:w-full flex flex-col text-white px-4">
+        <div class="bof-productions">
+            <div class="container mx-auto bg-four my-10 rounded-lg py-6">
+                <div class="githunguri-psycho w-full flex flex-col-reverse">
+                    <div class="px-6 text-justify w-full">
+                        <div class="flex flex-col lg:flex-row lg:justify-between w-full lg:items-center mb-4">
+                            <div class="w-full lg:w-11/12 flex items-center">
+                                <h1 class="text-[0.6rem] lg:text-xl text-start font-bold psycho">Githunguri Psycho</h1>
+                            </div>
+                            <div class="w-full lg:w-1/12 flex items-center">
+                                <img src="./media/bof.white.PNG" alt="" class="w-1/4 lg:w-full">
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-base">
+                                Drawing from my deep experience as a graphic designer, I crafted promotional materials for the chilling short film, "Githunguri Psycho" set in the haunting ambiance of Githunguri. Integrating visual storytelling techniques, I mirrored the film's intertwining of myths and grim realities within the poster's design. Utilizing a carefully selected color palette, I echoed the narrative's dread and suspense, complemented by typography that resonates with its haunting undertones. Through balanced composition, I emphasized Githunguri's eerie character, ensuring every intricate detail captured the gripping tension of the film, making it an irresistible lure for potential viewers.
+                            </p>
+                        </div>
+                    </div>
+                    <section class="section-swiper ">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/bof/githunguri-psycho2.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/bof/githunguri-psycho1.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/bof/githunguri-psycho3.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/bof/githunguri-psycho4.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/bof/githunguri-psycho5.JPG" alt="">
+                                </div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <div class="aiesec-work">
+            <div class="container mx-auto bg-five my-10 rounded-lg py-6">
+                <div class="Gala Dinner w-full flex flex-col-reverse">
+                    <div class="px-6 text-justify w-full">
+                        <div class="flex flex-col lg:flex-row lg:justify-between w-full lg:items-center mb-4">
+                            <div class="w-full lg:w-5/6 flex items-center">
+                                <h1 class="text-2xl lg:text-3xl text-start font-bold ">Gala Dinner 2023</h1>
+                            </div>
+                            <div class="w-1/2 lg:w-1/6 flex items-center">
+                                <img src="./media/Blue-Logo.png" alt="" class="w-full">
+                            </div>
+                        </div>
+                        <p class="text-base">
+                            These Gala Dinner posters embody my creative journey, a tapestry woven with passion and imagination. From the first stroke to the final design, every element reflects my commitment to capturing the essence of elegance. Just as a gala brings diverse minds together, each detail on these posters has been curated to resonate with sophistication and allure. Through countless drafts and inspired choices, I've poured a piece of my artistic spirit into each poster, hoping to evoke excitement and anticipation for a night of grandeur. Join me in celebrating the culmination of this journey, as we await a memorable evening of splendor and shared moments. 🎨✨
+                        </p>
+                    </div>
+                    <section class="section-swiper ">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/Gala Dinner Theme Reveal_Final Poster Main 2.png" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/Gala Dinner Theme Reveal_Final Poster 2 Alternative 1.png" alt="">
+                                </div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <div class="container mx-auto bg-five my-10 rounded-lg py-6">
+                <div class="Leadershipw-full flex flex-col-reverse">
+                    <div class="px-6 text-justify w-full">
+                        <div class="flex flex-col lg:flex-row lg:justify-between w-full lg:items-center mb-4">
+                            <div class="w-full lg:w-5/6 flex items-center">
+                                <h1 class="text-2xl lg:text-3xl text-start font-bold ">Embrace the Leader Within</h1>
+                            </div>
+                            <div class="w-1/2 lg:w-1/6 flex items-center">
+                                <img src="./media/Blue-Logo.png" alt="" class="w-full">
+                            </div>
+                        </div>
+                        <p class="text-base">
+                            These posters draw inspiration from the iconic Jon Snow, showcasing essential leadership traits. As a reluctant yet impactful leader, Jon demonstrates the power of humility, empathy, and the willingness to learn from failures. Just as he unites diverse groups for a common cause, effective leadership requires collaboration and the ability to inspire others. Through these posters, we celebrate the journey of self-discovery and growth that every leader undertakes. Whether on the battlefield or in the boardroom, let Jon Snow's legacy remind us that true leadership is born from authenticity and the dedication to serve. 🛡️👑
+                        </p>
+                    </div>
+                    <section class="section-swiper ">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/got-leadership1.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/got-leadership3.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/got-leadership4.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/got-leadership5.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/got-leadership2.PNG" alt="">
+                                </div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <div class="container mx-auto bg-five my-10 rounded-lg py-6">
+                <div class="mental-healthw-full flex flex-col-reverse">
+                    <div class="px-6 text-justify w-full">
+                        <div class="flex flex-col lg:flex-row lg:justify-between w-full lg:items-center mb-4">
+                            <div class="w-full lg:w-5/6 flex items-center">
+                                <h1 class="text-2xl lg:text-3xl text-start font-bold ">The Journey to Mental Health Wellness</h1>
+                            </div>
+                            <div class="w-1/2 lg:w-1/6 flex items-center">
+                                <img src="./media/Blue-Logo.png" alt="" class="w-full">
+                            </div>
+                        </div>
+                        <p class="text-base">
+                            These posters represent my heartfelt commitment to raising awareness about mental health. Through my creative journey, I've discovered that good mental health is a vital part of our overall well-being. It's about acknowledging our struggles without judgment and seeking help when needed. Just as brushstrokes come together to form a beautiful painting, self-care, compassion, and resilience weave the fabric of good mental health. Let's spread understanding, erase stigma, and remind everyone that they're not alone on this path. Together, we can create a world that nurtures and supports every individual's mental health. 🌱
+                        </p>
+                    </div>
+                    <section class="section-swiper ">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-1.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-2.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-4.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-5.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-6.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-7.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/aiesec/mental-health-8.PNG" alt="">
+                                </div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <div class="freelance-work">
+            <div class="container mx-auto bg-six my-10 rounded-lg py-6">
+                <div class="Unburden Initiative w-full flex flex-col-reverse">
+                    <div class="px-6 text-justify w-full">
+                        <div class="flex flex-col lg:flex-row lg:justify-between w-full lg:items-center mb-4">
+                            <div class="w-full lg:w-5/6 flex items-center">
+                                <h1 class="text-2xl lg:text-3xl text-start font-bold ">Unburden Initiative</h1>
+                            </div>
+                            <div class="w-1/2 lg:w-1/6 flex items-center">
+                                <img src="" alt="" class="w-full">
+                            </div>
+                        </div>
+                        <p class="text-base">
+                            A captivating series of posters crafted for the Unburden Initiative, a commendable NGO dedicated to championing mental health awareness. These visual masterpieces not only represent the organization's fervent passion for supporting individuals navigating mental health challenges but also aim to inspire a wider audience to understand, empathize, and engage with the cause. Through powerful imagery and poignant messaging, each poster serves as a beacon of hope, fostering a world where mental well-being is recognized, respected, and celebrated.
+                        </p>
+                    </div>
+                    <section class="section-swiper ">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-7.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-4.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-5.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-6.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-8.PNG" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-1.png" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-2.png" alt="">
+                                </div>
+                                <div class="swiper-slide lg:w-1/5 w-7/12">
+                                    <img src="Graphics/others/unburden-initiative-3.png" alt="">
+                                </div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<script src="main.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.5/swiper-bundle.min.js'></script>
+<script  src="./slider/script.js"></script>
+
+</html>
---
+++ partials/nav.php
@@ -0,0 +1,137 @@
+<?php
+
+$number = rand(1, 6);
+
+switch($number) {
+    case 1:
+        $color = "one";
+        break;
+    case 2:
+        $color = "two";
+        break;
+    case 3:
+        $color = "three";
+        break;
+    case 4:
+        $color = "four";
+        break;
+    case 5:
+        $color = "five";
+        break;
+    case 6:
+        $color = "six";
+        break;
+    default:
+        $color = "black";
+        break;
+}
+?>
+
+<header class="sticky top-0 z-50">
+        <nav class="nav-bar flex flex-row justify-between bg-<?= $color; ?> w-full p-4 text-white items-center">
+            <div class="nav-brand flex w-1/3">
+                <a href="index.php">
+                    <h2 class="text-xl md:text-xl text-white">
+                        <?php if ($title=="graphics") echo 'danroy.psd' ;elseif ($title=="video") echo 'danroy.mov';else echo 'danroy.dev';?>
+                    </h2>
+                </a>
+            </div>
+            <div class="md:hidden w-2/3 flex justify-end h-8">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cursor-pointer w-8 burger-icon">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cursor-pointer w-8 hidden close-icon">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>              
+            </div>
+            
+            <div class="nav-links hidden md:flex md:items-center md:w-2/3 md:flex-row md:justify-around">
+                <div class="nav-link">                   
+                    <a href="software.php" title="Software development" class="flex flex-row items-center">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                            </svg>
+                        </div>
+                        <div>
+                            <small class="">
+                                Software Dev
+                            </small>
+                        </div>
+                    </a>
+                </div>
+                <div class="nav-link">                   
+                    <a href="" title="Cinematography development" class="construction flex flex-row items-center">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
+                            </svg>
+                        </div>
+                        <div>
+                            <small class="">
+                                Cinematography
+                            </small>
+                        </div>
+                    </a>
+                </div>
+                <div class="nav-link">                   
+                    <a href="graphics-design.php" title="Graphics design" class="flex flex-row items-center">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                            </svg>
+                        </div>
+                        <div>
+                            <small class="">
+                                Graphics Design
+                            </small>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <div class="h-200 mobile-nav-links bg-<?= $color?> text-white text-center w-full hidden md:hidden">
+            <div class="p-2 flex justify-center">
+                <a href="software.php" title="Software development" class="flex flex-row items-center space-x-4">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                        </svg>
+                    </div>
+                    <div>
+                        <small class="">
+                            Software Dev
+                        </small>
+                    </div>
+                </a>
+            </div>
+            <div class="p-2 flex justify-center">
+                <a href="" title="Software development" class="construction flex flex-row items-center space-x-4">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
+                        </svg>
+                    </div>
+                    <div>
+                        <small class="">
+                            Cinematography
+                        </small>
+                    </div>
+                </a>
+            </div>
+            <div class="p-2 flex justify-center">
+                <a href="graphics-design.php" title="Software development" class="flex flex-row items-center space-x-4">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                    </div>
+                    <div>
+                        <small class="">
+                            Graphics Design
+                        </small>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </header>
